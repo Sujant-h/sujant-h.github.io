450c700bcee08ab4a1ea51f072db232f7dabdaf6
Sujanth <84275282+Sujant-h@users.noreply.github.com>
2024-07-17 13:26:59 +0200

    Fix offline functionality of PWA Web App v5

--- powerpoint/sw.js
+++ powerpoint/sw.js
@@ -4,13 +4,9 @@ const CACHE_NAME = 'v1';
 const urlsToCache = [
     'index.html',
     'styles.css',
-	'pptxgen.bundle.js',
     'index.js',
-    'data.json', // Include the JSON file itself in the cache
-    '/images/vorlage/image1.jpg',
-    '/images/vorlage/image2.jpg',
-    '/images/vorlage/image3.jpg',
-    '/images/vorlage/image4.jpg',
+    'data.json',
+    'https://cdn.jsdelivr.net/npm/pptxgenjs@3.12.0/dist/pptxgen.bundle.js' // Include the JSON file itself in the cache
 ];
 
 // Installing Service Worker
@@ -18,38 +14,16 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-            .then(() => {
-                // Fetch the JSON file and cache image URLs dynamically
-                return fetch('data.json')
+                cache.addAll(urlsToCache)
+                    .then(() => fetch('data.json')) // Fetch the JSON file
                     .then(response => response.json())
                     .then(items => {
                         const imageUrls = items.flatMap(item => {
                             return item.filename.filter(f => f).map(f => `images/slides/${f}.png`); // Filter out empty filenames and map to full paths
                         });
-                        return caches.open(CACHE_NAME)
-                            .then(cache => cache.addAll(imageUrls));
+                        return cache.addAll(imageUrls); // Cache all non-empty image paths
                     });
             })
-            .catch(error => {
-                console.error('Failed to install Service Worker: ', error);
-            })
-    );
-});
-
-// Activate Service Worker
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
     );
 });
 
@@ -58,15 +32,8 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                return response || fetch(event.request).then(fetchResponse => {
-                    return caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, fetchResponse.clone());
-                        return fetchResponse;
-                    });
-                });
-            }).catch(() => {
-                // Fallback logic if the resource is not available in the cache or network
-                return caches.match('offline.html'); // You can provide a fallback HTML file for offline usage
+                // Return cached response if found, otherwise fetch from network
+                return response || fetch(event.request);
             })
     );
 });
